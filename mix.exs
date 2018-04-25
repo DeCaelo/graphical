8813886f655b474b76deee624c5082709c38b512
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-04-25 08:26:14 +0200

    add deps for user auth

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule Graphical.Mixfile do
       {:absinthe, "~> 1.4.6"},
       {:absinthe_plug, "~> 1.4.2"},
       {:absinthe_ecto, git: "https://github.com/absinthe-graphql/absinthe_ecto.git"},
-      {:faker, "~> 0.9"}
+      {:faker, "~> 0.9"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"},
+      {:guardian, "~> 1.0"}
     ]
   end
 
